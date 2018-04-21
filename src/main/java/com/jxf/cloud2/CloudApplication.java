3279d82c9559442a8b82e2ad100b36794983e887
jinxf <813439947@qq.com>
2018-04-21 17:44:43 +0800

    修改包名

--- src/main/java/com/jxf/cloud2/CloudApplication.java
+++ src/main/java/com/jxf/cloud2/CloudApplication.java
@@ -1,4 +1,4 @@
-package com.tsy.cloud;
+package com.jxf.cloud2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/jxf/cloud2/CloudApplicationTests.java
+++ src/test/java/com/jxf/cloud2/CloudApplicationTests.java
@@ -1,4 +1,4 @@
-package com.tsy.cloud;
+package com.jxf.cloud2;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
